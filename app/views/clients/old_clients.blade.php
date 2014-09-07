b1135d09f46fde9e69939c6ee6bdeed4275be22c
Jean-Marie Porchet <jm@jmporchet.ch>
2014-09-08 00:45:33 +0200

    =afficher les clients classés par les plus vieux

---
+++ app/views/clients/old_clients.blade.php
@@ -0,0 +1,19 @@
+@extends('_partials/master')
+@section('content')
+<h1>Liste des anciens clients <small><a href="{{ route('clients.create') }}" class="btn btn-success">nouveau client</a></small></h1>
+<table class="table table-striped table-condensed">
+    <thead>
+    <th>Nom</th>
+    <th><i class="glyphicon-cog glyphicon"></i> Action</th>
+    </thead>
+    @foreach ($clients as $client)
+    <tr>
+        <td><a href=" {{ route('clients.show', $client->id) }}">{{ $client->prenom }} {{ $client->nom }}</a></td>
+        <td>
+            <a href="{{ url('lecons/create', $client->id) }}" class="btn btn-success btn-xs"><i class="glyphicon glyphicon-plus-sign"></i> cours</a>
+            <a href="{{ route('clients.edit', $client->id) }}" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-pencil"></i> editer</a>
+        </td>
+    </tr>
+    @endforeach
+</table>
+@stop
